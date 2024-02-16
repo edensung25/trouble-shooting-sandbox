c5e86a37a4ff2903e2d7316f6d1805990fb0f9f7
csong <cai-cian.song@sony.com>
2024-02-16 09:44:32 -0800

    add web and try/catch

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 import {
   ApolloClient,
   ApolloProvider,
@@ -8,57 +8,79 @@ import {
   InMemoryCache,
   gql,
   useQuery,
+  from,
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { RestLink } from "apollo-link-rest";
 
-const ALL_PEOPLE = gql`
-  query AllPeople {
-    people {
-      id
+const query = gql`
+  query Luke {
+    person @rest(type: "Person", path: "people/1/") {
       name
     }
   }
 `;
 
+// const uri = "https://swapi.dev/api/";
+const uri = "http://does-not-exist-at-all.com/graphql";
+const restLink = new RestLink({ uri });
+
+const onErrorLink = onError(({ forward, operation }) => {
+  console.log("onErrorLink");
+  return forward(operation);
+});
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "http://does-not-exist-at-all.com/graphql"
-  }),
+  link: from([onErrorLink, restLink]),
 });
 
 const Component = () => {
-  const { loading, data, error } = useQuery(ALL_PEOPLE);
+  const { loading, data, error } = useQuery(query);
 
   function fetchWithClient() {
-    client.query({ query: ALL_PEOPLE });
-  };
-  
+    client.query({ query: query }).catch((e) => {
+      console.log("button catch ", error);
+    });
+  }
+
   useEffect(() => {
-    client.query({ query: ALL_PEOPLE });
+    try {
+      client
+        .query({ query: query })
+        .then((data) => {
+          console.log("data: ", data);
+        })
+        .catch((error) => {
+          console.log("useEffect catch ", error);
+        });
+    } catch (error) {
+      console.log("call with client useEffect: ", error);
+    }
   }, []);
 
   return (
-  <View style={styles.container}>
-    <Text>{error?.message}</Text>
-    <Button onPress={fetchWithClient} title="Fetch w/ Client" />
-    <StatusBar style="auto" />
-  </View>
+    <View style={styles.container}>
+      <Text>{error?.message}</Text>
+      <Button onPress={fetchWithClient} title="Fetch w/ Client" />
+      <StatusBar style="auto" />
+    </View>
   );
-}
+};
 
 export default function App() {
   return (
-  <ApolloProvider client={client}>
-    <Component />
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+      <Component />
+    </ApolloProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
